9fd1bc460b48cfc543f938e124c09e3fd353316d
juan-saracco <juansaracco1212@gmail.com>
2023-04-12 13:31:32 -0300

    react7

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,24 @@
-import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import { Route, Routes } from 'react-router-dom';
-import db from "../db/firebase-config.js";
-import "./App.css"
-import Footer from './components/Footer/index.jsx';
-import Home from './components/Home/index.jsx';
-import ItemDetail from './components/ItemDetail/index.jsx';
-import ListItems from './components/ListItems/index.jsx';
-import Navbar from './components/Navbar/index.jsx';
-import Title from './components/Title/index.jsx';
-import ItemListContainer from './components/ItemListContainer/index.jsx';
-import ItemDetailContainer from './components/ItemDetailContainer/index.jsx';
-//import CartProvider from './context/CartContext.jsx';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/index.jsx";
+import Navbar from "./components/Navbar/index.jsx";
+import ItemDetailContainer from "./components/ItemDetailContainer/index.jsx";
+import ItemListContainer from "./components/ItemListContainer/index.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  //state productos
-  const [productos, setProductos] = useState([])
-  //state carrito
-  const [carrito, setCarrito] = useState([])
-  //state loading
-  const [loading, setLoading] = useState(true)
-  //referencias a las colecciones
-  const productosCollectionRef = collection(db, "productos");
-  const carritoCollectionRef = collection(db, "carrito");
-
-  //funcion para traer los productos
-  const getItems = async () => {
-   const productosCollection = await getDocs(productosCollectionRef);
-   setProductos(
-     productosCollection.docs.map((doc) => ({
-    ...doc.data(), id: doc.id }))
-   );
-    setLoading(false);
-  }
-
-  const deleteItem = async (id) => {
-    const docRef = doc(db, "productos", id)
-    await deleteDoc(docRef);
-    getItems();
-  }
-
-
-  //funcion para agregar productos a la coleccion carrito
-  const addItem = async () => {
-    const docRef = await addDoc(carritoCollectionRef, {
-    title: "Producto 1(additem)",
-    });
-    console.log("Document written with ID: ", docRef.id);
-    }
-
-  //funcion para agregar productos a la coleccion productos
-   useEffect(() => {
-    getItems();
-  }, []);
-
-  if (loading) {
-    return <div className="loading"><h1 className='loader'></h1></div>
-  }
-
   return (
-    <div>
-
+    <>
       <Navbar />
-      <Title />
-      <ItemDetailContainer />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route 
-         path="/productos"
-         element={
-          <ListItems 
-          productos={productos}
-           setProductos={setProductos} 
-           deleteItem={deleteItem} 
-           />
-          }
-         />
-         <Route 
-         path='/productos/:id'
-          element={<ItemDetail />}
-          />
-       </Routes>
-
-       <Footer />
-      </div>
-  )
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/categoria" element={<ItemListContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/detalle" element={<ItemDetailContainer />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
-
 
 export default App;
--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -6,46 +6,17 @@ import styles from './itemDetail.module.css';
 //import { useCartContext } from '../../context/CartContext.jsx';
 
 const ItemDetail = ({ data }) => {
-   const {id} = useParams();
-   const [item, setItem] = useState({});
-   const [loading, setLoading] = useState(true)
-   //const {addProduct} = useCartContext();
-
-   const getItem = async (id) => {
-    const docRef = doc(db, "productos", id);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-        setItem(docSnap.data());
-    } else {
-        console.log("No such document!");}
-        setLoading(false);
-    }
-
-
-    useEffect(() => {
-        getItem(id);
-    }, [id]);
-
-    if (loading) {
-      return <div className="loading"><h1 className='loader'></h1></div>
-    }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.itemContainer}>
-      <img className={styles.img} src={item.img}></img>
-        <h3 className={styles.title}>{item.title}</h3>
-        <h2>${item.price}</h2>
-        <h3>{item.description}</h3>
-      </div>
-      <button className={styles.button}>Agregar al carrito</button>
-      
+
 
       <div>
         <div>
+          <h1>{data.title}</h1>
+          <h2>{data.director}</h2>
         </div>
       </div>
-    </div>
+
   )
 }
 
