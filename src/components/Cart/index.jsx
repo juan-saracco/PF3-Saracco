3de16e0c35ab792167c3632034c0d233e626a258
juan-saracco <juansaracco1212@gmail.com>
2023-04-19 17:08:57 -0300

    funcionalidad lista

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import { useCartContext } from '../../context/CartContext'
 import { Link } from 'react-router-dom';
 import ItemCart from '../ItemCart';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 const Cart = () => {
   const {cart, totalPrice} = useCartContext();
+
+  const order = {
+    buyer: {
+      name: "Juan",
+      phone: "123456789",
+      email: "juan@gmail.com",
+      adrress: "Calle falsa 123"
+    },
+    items: cart.map(product => ({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      quantity: product.quantity
+      })),
+    total: totalPrice(),
+  }
+
+  const handleCick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order)
+    .then(({id}) => console.log(id))
+  }
+
+
 
   if(cart.length === 0){
 
@@ -22,6 +48,7 @@ const Cart = () => {
     <div>
       {cart.map(product => <ItemCart key={product.id} product={product}/>)}
       <h1>Total: ${totalPrice()}</h1>
+      <button onClick={handleCick} >Comprar</button>
     </div>
   )
 }
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Title = ({greeting}) => {
-    console.log(greeting);
+
   return (
     <h1>{greeting}</h1>
   )
